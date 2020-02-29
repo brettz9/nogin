70fd4a16cd615a78d4ca6b3a336c3ef1725cc633
Brett Zamir <brettz9@yahoo.com>
2020-02-29 09:29:33 +0800

    - Add sample `node-login.js`

---
+++ node-login-sample.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
+  NL_EMAIL_HOST: 'subdomain.domain.com',
+  NL_EMAIL_USER: 'me@example.name',
+  NL_EMAIL_PASS: 'abc123456',
+  NL_EMAIL_FROM: 'Node Login <do-not-reply@gmail.com>',
+  NS_EMAIL_TIMEOUT: 20000
+};
