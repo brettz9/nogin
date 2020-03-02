0be10fc5a4ad7d83275484ae5e1295a6fc6c282b
Brett Zamir <brettz9@yahoo.com>
2020-03-02 17:55:47 +0800

    - Use envelope for email parsing; more logging/info

--- email.js
+++ email.js
@@ -2,6 +2,7 @@
 
 const Pop3Command = require('node-pop3');
 const getStream = require('get-stream');
+const Envelope = require('envelope');
 
 const {
   NL_EMAIL_HOST,
@@ -33,13 +34,21 @@ try {
     pop3.command('STAT'), // no args
 
     // Retrieval of message
-    pop3.command('RETR', 2) // requires msg number (and returns stream)
+    pop3.command('RETR', 4) // requires msg number (and returns stream)
   ]);
 
-  console.log(statInfo); // 100 102400
-  console.log(retrInfo); // 1024 octets
+  console.log('statInfo', statInfo); // 100 102400
+  console.log('retrInfo', retrInfo); // 1024 octets
 
-  console.log('getStream', await getStream(retrStream));
+  const retrStreamString = await getStream(retrStream);
+  console.log('retrStream', retrStreamString);
+  const parsedRetrStream = new Envelope(retrStreamString);
+  console.log('retrStream parsed', parsedRetrStream);
+
+  console.log(
+    'parsedRetrStream.[1].header.contentType',
+    parsedRetrStream[1].header.contentType
+  );
 
   // Mark message as deleted
   // const [deleInfo] = await pop3.command('DELE', 1); // requires msg number
@@ -57,15 +66,18 @@ try {
   const [
     listInfo, listStream
   // Takes optional msg number (and returns stream)
-  ] = await pop3.command('LIST');
+  ] = await pop3.command('LIST', 4);
   console.log('listInfo', listInfo);
-  console.log('listStream', await getStream(listStream));
+  const listStreamString = await getStream(listStream);
+  console.log('listStream', listStreamString);
+
+  console.log('listStream parsed', await new Envelope(listStreamString));
 
   // 1. TOP (required msg number and required non-negative number of line)
   //    - i.e., get specific message; returns stream
   const [topInfo, topStream] = await pop3.command(
     'TOP', 2, 10000
-  ); // Takes optional msg number
+  );
   console.log('topInfo', topInfo);
   console.log('topStream', await getStream(topStream));
 
@@ -95,17 +107,19 @@ try {
   // 4. PASS (required server/mailbox-specific string)
   // 5. APOP (required mailbox string and MD5 digest string)
   //    - i.e., alternate authentication to USER/PASS exchange
+  //    - (could look at https://github.com/ditesh/node-poplib )
 
   // Commands from https://tools.ietf.org/html/rfc5034
   // 1. AUTH (required mechanism and optional initial-response)
 
   // Commands from https://tools.ietf.org/html/rfc2595
-  // 1. STLS (no args)
+  // 1. STLS (no args); could look at https://github.com/ditesh/node-poplib
   // 1. STARTTLS (no args)
 
   // Commands from https://tools.ietf.org/html/rfc2449
-  // 1. CAPA (no args)
-  // 2. SASL, RESP-CODES, LOGIN-DELAY, PIPELINING, EXPIRE
+  // 1. CAPA (no args) (could look at https://github.com/ditesh/node-poplib )
+  // 2. SASL (could look at https://github.com/ditesh/node-poplib)
+  // 3. RESP-CODES, LOGIN-DELAY, PIPELINING, EXPIRE
   //    and IMPLEMENTATION
 
   // Removes all messages marked as deleted; removes any lock
