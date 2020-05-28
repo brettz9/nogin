c8019c85b87a88d372403c262f27347e916ca516
Brett Zamir <brettz9@yahoo.com>
2020-05-28 11:45:54 +0800

    - Linting: Check `no-extend-native` - Linting: More granular ignores by use of `overrides` instead of ignore file (also avoids warnings that files added by `es-file-traverse` are being ignored)

--- .3rdparty-eslintrc.js
+++ .3rdparty-eslintrc.js
@@ -2,8 +2,27 @@
 
 module.exports = {
   parser: 'babel-eslint',
+  overrides: [
+    {
+      files: [
+        'node_modules/bson/lib/bson/parser/deserializer.js',
+        'node_modules/depd/index.js',
+        'node_modules/es-abstract/GetIntrinsic.js'
+      ],
+      rules: {
+        'no-eval': 'off'
+      }
+    },
+    {
+      files: ['node_modules/once/once.js'],
+      rules: {
+        'no-extend-native': 'off'
+      }
+    }
+  ],
   rules: {
     // Intrusive
+    'no-extend-native': ['error'],
     'no-global-assign': ['error'],
 
     // Vulnerable
