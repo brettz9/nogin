cb054a59ffb8b4b8b05f2d02633508bd0ea2c693
Brett Zamir <brettz9@yahoo.com>
2021-01-27 14:56:44 +0800

    - npm: Update devDep. (`integrity-matters`) - Todo

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,9 @@
 
 module.exports = {
   reject: [
+    // Todo:
+    // Todo[bootstrap@>5.0.0-beta1]: beta1 wasn't at https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-beta1/css/bootstrap.min.css
+
     // Lock in any npm packages here
     // Todo[bootstrap@>5.0.0-alpha1]: Update if bootstrap updates the version;
     //  see https://github.com/twbs/bootstrap/blob/master/config.yml
