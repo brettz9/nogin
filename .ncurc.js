f72eb3b4fda27f1f15977d285b54b4ca970a86dc
Brett Zamir <brettz9@yahoo.com>
2021-01-27 15:42:53 +0800

    - Todo

--- .ncurc.js
+++ .ncurc.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   reject: [
-    // Todo:
     // Todo[bootstrap@>5.0.0-beta1]: beta1 wasn't at https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-beta1/css/bootstrap.min.css
+    // Even 4.5.3 wasn't on the CDN
 
     // Lock in any npm packages here
     // Todo[bootstrap@>5.0.0-alpha1]: Update if bootstrap updates the version;
