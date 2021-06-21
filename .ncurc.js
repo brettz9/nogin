3966ffa9258b6b7b8d9182f1c4ea93a2d6fd53f2
Brett Zamir <brettz9@yahoo.com>
2021-06-21 21:15:31 +0800

    - npm: Update `file-fetch` (minor) - npm: Update devDeps.

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,9 @@
 
 module.exports = {
   reject: [
+    // Switch when ready to transition to ESM
+    'command-line-basics',
+    'es-file-traverse',
     // Lock in any npm packages here
     // Todo[bootstrap@>5.0.1]: Update only if bootstrap updates the
     //  version; see https://github.com/twbs/bootstrap/blob/master/config.yml
