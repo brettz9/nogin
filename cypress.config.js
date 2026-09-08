d9d38fe3c282bc5879dd1a73152613546f6fdff8
Brett Zamir <brettz9@yahoo.com>
2026-09-08 09:43:22 -0700

    chore: avoid deprecated cypress property

--- cypress.config.js
+++ cypress.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
   env: {
     disableEmailChecking: true
   },
-  allowCypressEnv: false,
   video: false,
   e2e: {
     // We've imported your old cypress plugins here.
