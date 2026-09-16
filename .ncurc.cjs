50482f24a686a5db430aeb9dafa8e73ec62aa553
Brett Zamir <brettz9@yahoo.com>
2026-09-15 21:12:47 -0700

    chore: update file-fetch, intl-dom, devDeps.

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -2,10 +2,6 @@
 
 module.exports = {
   reject: [
-    // Fixing version of `file-fetch` to allow working with jsdom-overwritten
-    //   global `URL`s as needed in typeson-registry (intl-dom?)
-    'file-fetch'
-
     // Todo[bootstrap@>5.3.8]: Update only if bootstrap updates the
     //  version; see https://github.com/twbs/bootstrap/blob/main/config.yml
     // '@popperjs/core'
