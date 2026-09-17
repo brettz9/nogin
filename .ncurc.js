6489f3d172dce8663d1a6021a2e1cd3d27e5b4ab
Brett Zamir <brettz9@yahoo.com>
2026-09-16 21:51:00 -0700

    chore: update jsdom, ncurc format

--- .ncurc.js
+++ .ncurc.js
@@ -1,6 +1,4 @@
-'use strict';
-
-module.exports = {
+export default {
   reject: [
     // Todo[bootstrap@>5.3.8]: Update only if bootstrap updates the
     //  version; see https://github.com/twbs/bootstrap/blob/main/config.yml
